5d4004cbaed0a6b14a693f0af95ca4b9a43e5280
Ramón HC <ramon@stage.tech>
2018-06-30 02:22:29 +0200

    1 - on boarding: pagination with dots into container

--- bnc10/scenes/onboarding/OnboardingPageViewController.swift
+++ bnc10/scenes/onboarding/OnboardingPageViewController.swift
@@ -1,30 +1,99 @@
-//
-//  OnboardingPageViewController.swift
-//  bnc10
-//
-//  Created by Ramón HC on 29/6/18.
-//  Copyright © 2018 Ramón HC. All rights reserved.
-//
-
 import UIKit
 
-class OnboardingPageViewController: UIPageViewController {
-
+class OnboardingPageViewController: UIPageViewController, UIPageViewControllerDelegate,  UIPageViewControllerDataSource  {
+    
+    // MARK: Properties
+    var pageControl = UIPageControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         dataSource = self
-        // Do any additional setup after loading the view.
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+        self.delegate = self
+        configurePageControl()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Dots
+    func configurePageControl() {
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = orderedViewControllers.count
+        self.pageControl.currentPage = 0
+        self.pageControl.alpha = 0.5
+        self.pageControl.tintColor = UIColor.black
+        self.pageControl.pageIndicatorTintColor = UIColor.white
+        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
+    }
+    
+    // MARK: Delegate functions
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+    
     // MARK: UIPageViewControllerDataSource
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
     
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+
+
     
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [
+            self.getNewViewController(storyboardId: "onboardingPage0"),
+            self.getNewViewController(storyboardId: "onboardingPage1"),
+            self.getNewViewController(storyboardId: "onboardingPage2")
+        ]
+    }()
+    
+    private func getNewViewController(storyboardId: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier:storyboardId)
+    }
     
     /*
     // MARK: - Navigation
--- bnc10/scenes/onboarding/OnboardingViewController.swift
+++ bnc10/scenes/onboarding/OnboardingViewController.swift
@@ -1,25 +1,18 @@
-//
-//  ViewController.swift
-//  bnc10
-//
-//  Created by Ramón HC on 29/6/18.
-//  Copyright © 2018 Ramón HC. All rights reserved.
-//
-
 import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
