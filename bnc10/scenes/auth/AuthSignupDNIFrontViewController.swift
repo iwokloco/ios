0f5bd0cee03b5612000438dbc96507c4fdd70198
Ramón HC <iwokloco@macbook-pro-de-ramon.local>
2018-07-09 02:09:01 +0200

    Pick images from photo album

--- bnc10/scenes/auth/AuthSignupDNIFrontViewController.swift
+++ bnc10/scenes/auth/AuthSignupDNIFrontViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
-class AuthSignupDNIFrontViewController: UIViewController {
+class AuthSignupDNIFrontViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -12,6 +15,23 @@ class AuthSignupDNIFrontViewController: UIViewController {
     
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // use the original image
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: Actions
     
     @IBAction func goBack(_ sender: UIButton) {
@@ -20,5 +40,12 @@ class AuthSignupDNIFrontViewController: UIViewController {
     
     @IBAction func doRegistration(_ sender: UIButton) {
         self.performSegue(withIdentifier: "segueSignupDNIToStep2", sender: nil)
+    }
+
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
